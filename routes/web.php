9cab12b2e8073df6de5352c6b987dad3c7af3412
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-07 16:51:50 +0100

    sitting Laravel environment

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DetailsController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ForgotPWController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPWController;
+use App\Http\Controllers\EventController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
