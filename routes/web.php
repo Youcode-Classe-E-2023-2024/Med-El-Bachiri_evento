bffe3dfe84a7eca6d5854aa04bb667cd34b3e5e1
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-09 20:32:52 +0100

    updating routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DetailsController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ForgotPWController;
 use App\Http\Controllers\Auth\LoginController;
@@ -43,9 +46,27 @@ Route::middleware('guest')->group(function () {
 // logout
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-Route::middleware('auth')->group(function (){
+Route::middleware('auth')->group(function () {
+    Route::post('/reserve_ticket/{event_id}', [TicketController::class, 'reserve_ticket']);
     Route::post('/event/create', [EventController::class, 'create']);
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::put('/valid_event/{event_id}', [EventController::class, 'valid']);
-    Route::put('/not_valid_event/{event_id}', [EventController::class, 'not_valid']);
+
+    Route::middleware('role:Admin')->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::put('/valid_event/{event_id}', [EventController::class, 'valid']);
+        Route::put('/update_user_role/{user_id}', [UserController::class, 'update_role']);
+        Route::delete('/delete_user/{user_id}', [UserController::class, 'delete_user']);
+        // category mng
+        Route::post('/category/create', [CategoryController::class, 'create']);
+        Route::delete('/category/{category_id}/destroy', [CategoryController::class, 'destroy']);
+        Route::put('/category/update', [CategoryController::class, 'update']);
+    });
+
+    Route::middleware('role:Admin|Organizer')->group(function () {
+        Route::put('/not_valid_event/{event_id}', [EventController::class, 'not_valid']);
+        Route::get('/my/events', [EventController::class, 'my_events']);
+        Route::get('/event/{event_id}/edit', [EventController::class, 'edit']);
+        Route::put('/event/{event_id}/update', [EventController::class, 'update']);
+        Route::put('/ticket/{ticket_id}/approve', [TicketController::class, 'approve']);
+        Route::delete('/ticket/{ticket_id}/deny', [TicketController::class, 'deny']);
+    });
 });
