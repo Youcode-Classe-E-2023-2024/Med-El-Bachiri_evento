80fdb5af31a289d72688479f1a5d2af9472c6c12
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-09 20:24:17 +0100

    return 404 if event does not exist

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -12,7 +12,10 @@ class DetailsController extends Controller
         $event = Event::where('id', $request->event_id)
             ->with(['category:id,name', 'user:id,name,email'])
             ->first();
-//        dd($event);
+
+        if ($event === null) {
+            return abort(404);
+        }
         return view('pages.details', ['event' => $event]);
     }
 }
