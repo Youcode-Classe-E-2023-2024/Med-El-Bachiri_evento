aba4349b31b3407c1b0812e1243ccba35c65c6a7
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-10 15:32:20 +0000

    api routes for filter and search

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\FilterController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/events/{category_id}', [FilterController::class, 'handle']);
+Route::post('/search/event/', [SearchController::class, 'handle']);
